e1d938e66acd8550cefc8c9e2088b5483d89bba0
Al Tobey <tobert@gmail.com>
2014-07-04 04:19:13 -0700

    example 2-player application using F7U12

---
+++ public/js/app.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2014 Albert P. Tobey <atobey@datastax.com> @AlTobey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * requires: jQuery swipe plugin and D3
+ */
+
+$(function() {
+  // create player 1's game board
+  var p1 = new F7U12(4, "#player1-game");
+      p1.init(2);
+
+  // create player 2's game board
+  var p2 = new F7U12(4, "#player2-game");
+      p2.init(2);
+
+  // disable default swipe events (e.g. ios overscroll)
+  document.body.addEventListener('touchmove', function(event) { event.preventDefault(); }, false);
+
+  // wire the jquery swipe plugin for player 1
+  $("#player1-game").swipe({
+    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+      console.log("player 1 swiped ", direction);
+      p1.move(direction);
+    }
+  });
+
+  // wire the jquery swipe plugin for player 2
+  $("#player2-game").swipe({
+    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+      console.log("player 2 swiped ", direction);
+      p2.move(direction);
+    }
+  });
+});
