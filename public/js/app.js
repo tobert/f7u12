2755d796b49209198d7dd1f97c210222290826eb
Al Tobey <tobert@gmail.com>
2014-07-06 16:46:44 -0700

    tighten up code, remove copy/paste bits

--- public/js/app.js
+++ public/js/app.js
@@ -17,40 +17,27 @@
  */
 
 $(function() {
-  // create player 1's game board
-  var p1 = new F7U12(4, "#player1-game");
-      p1.init(2);
-
-  // create player 2's game board
-  var p2 = new F7U12(4, "#player2-game");
-      p2.init(2);
-
   // disable default swipe events (e.g. ios overscroll)
   document.body.addEventListener('touchmove', function(event) { event.preventDefault(); }, false);
 
-  // wire the jquery swipe plugin for player 1
-  $("#player1-game").swipe({
-    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-      console.log("player 1 swiped ", direction);
-      p1.move(direction);
-    }
+  var move = function (p, dir) {
+      var data = p.move(dir);
+      console.log("Move Data: ", data);
+  };
+
+  // make two grids with controls
+  [1, 2].forEach(function (pnum) {
+    var target = "#player" + pnum + "-container";
+    var player = new F7U12(4, target); // 4x4 grid
+    player.init(2); // start with 2 tiles
+
+    // jquery swipe plugin
+    $(target).swipe({ swipe: function(e, direction) { move(player, direction); } });
+
+    // a simple dpad for testing with the mouse
+    ["up", "down", "left", "right"].forEach(function (dir) {
+      $(target + " #" + dir).on("click", function () { move(player, dir); });
+    });
+
   });
-
-  $("#player1-dpad #up"   ).on("click", function (e) { p1.move("up"); });
-  $("#player1-dpad #down" ).on("click", function (e) { p1.move("down"); });
-  $("#player1-dpad #left" ).on("click", function (e) { p1.move("left"); });
-  $("#player1-dpad #right").on("click", function (e) { p1.move("right"); });
-
-  // wire the jquery swipe plugin for player 2
-  $("#player2-game").swipe({
-    swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-      console.log("player 2 swiped ", direction);
-      p2.move(direction);
-    }
-  });
-
-  $("#player2-dpad #up"   ).on("click", function (e) { p2.move("up"); });
-  $("#player2-dpad #down" ).on("click", function (e) { p2.move("down"); });
-  $("#player2-dpad #left" ).on("click", function (e) { p2.move("left"); });
-  $("#player2-dpad #right").on("click", function (e) { p2.move("right"); });
 });
