3e48dfc8e1ee2b5773c10afdd024d9605225beb9
Al Tobey <tobert@gmail.com>
2014-07-19 10:09:55 -0700

    add comment

--- spark/src/main/scala/com.datastax/f7u12/main.scala
+++ spark/src/main/scala/com.datastax/f7u12/main.scala
@@ -15,6 +15,9 @@ package com.datastax.f7u12
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Note: being my first serious attempt at both Scala and Spark,
+ * this is probably not idiomatic Spark or Scala code
  */
 
 import org.apache.spark.{SparkConf, SparkContext}
