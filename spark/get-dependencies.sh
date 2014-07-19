845301fe4f3a0e43007fc3c83ad399c78b69b032
Al Tobey <tobert@gmail.com>
2014-07-19 12:26:57 -0700

    add my name to license block

--- spark/get-dependencies.sh
+++ spark/get-dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+#
+# Copyright 2014 Albert P Tobey <atobey@datastax.com> @AlTobey
+#
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
