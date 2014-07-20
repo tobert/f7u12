d82724313629f559e6ca7849905f8ac91bf572f5
Al Tobey <tobert@gmail.com>
2014-07-20 00:02:57 -0700

    fill in dashboard with placeholders
    These are not final graphs. I'm checking out dimple.js and working out
    which graphs should be on the page.

--- public/js/dashboard-app.js
+++ public/js/dashboard-app.js
@@ -17,6 +17,40 @@
 $(function() {
   document.body.addEventListener('touchmove', function(event) { event.preventDefault(); }, false);
 
+  // placeholders from dimplejs.org
+  var svg = dimple.newSvg("#graph3", 590, 400);
+  d3.tsv("/example_data.tsv", function (data) {
+    var myChart = new dimple.chart(svg, data);
+    myChart.setBounds(75, 30, 480, 330)
+    myChart.addMeasureAxis("x", "Unit Sales");
+    var y = myChart.addCategoryAxis("y", "Month");
+    y.addOrderRule("Date");
+    myChart.addSeries(null, dimple.plot.bar);
+    myChart.draw();
+  });
+
+  [1,2].forEach(function (d,i) {
+    var svg = dimple.newSvg("#graph"+(1+i), 590, 400);
+    d3.tsv("/example_data.tsv", function (data) {
+      data = dimple.filterData(data, "Owner", ["Aperture", "Black Mesa"])
+      var myChart = new dimple.chart(svg, data);
+      myChart.setBounds(60, 30, 430, 330);
+      var x = myChart.addCategoryAxis("x", ["Owner", "Month"]);
+      x.addGroupOrderRule("Date");
+      myChart.addMeasureAxis("y", "Unit Sales");
+      var s = myChart.addSeries(["Brand"], dimple.plot.line);
+      s.barGap = 0.05;
+      myChart.addLegend(510, 20, 100, 300, "left");
+      myChart.draw();
+    });
+  });
+
+  var game = new F7U12(4);
+      game.render("#grid1");
+
+  var game = new F7U12(4);
+      game.render("#grid2");
+
   var human_leaderboard = new F7U12.Leaderboard("#human_topN_leaderboard", "Human Top 10");
   var ai_leaderboard = new F7U12.Leaderboard("#ai_topN_leaderboard", "AI Top 10");
 
